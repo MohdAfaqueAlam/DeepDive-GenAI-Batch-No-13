2b5acd4e10f9bc72f2640352b9434a4d6471f8b6
GlobalProject <afaquejnu@gmail.com>
2025-11-09 19:46:52 +0530

    new addition Gradio

--- langchain_google_demo.py
+++ langchain_google_demo.py
@@ -1,10 +1,57 @@
-from langchain_google_genai import GoogleGenerativeAI
-from dotenv import load_dotenv
+# from langchain_google_genai import GoogleGenerativeAI
+# from dotenv import load_dotenv
 
+# load_dotenv()
+
+# llm = GoogleGenerativeAI(model="gemini-2.5-pro")
+
+# response = llm.invoke("Can you write a poem about a sunset in 50 words?")
+
+# print(response)
+
+# create a basic model to generate text using LangChain and Google Gemini 2.5 Flash
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+import streamlit as st
+
+from dotenv import load_dotenv  
 load_dotenv()
 
-llm = GoogleGenerativeAI(model="gemini-2.5-pro")
+#---------------------------Streamlit Interface---------------------------#
+st.title("Google Gemini 2.5 Flash Poem Generator")
+st.header("Generate a Poem about a Sunset")
+st.write("This app uses Google Gemini 2.5 Flash model to generate a poem about a sunset based on your input prompt.")
 
-response = llm.invoke("Can you write a poem about a sunset in 50 words?")
+#---------------------------LangChain Google Gemini 2.5 Flash Integration---------------------------#
 
-print(response)
+topic = st.text_input("Enter your topic:", "Can you write a poem about a sunset in 50 words?")
+WordCount = st.slider("Select the word count for the poem:", 10, 100, 50)
+
+#---------------------------Generate Poem on Button Click---------------------------#
+if st.button("Generate Poem"):
+     prompt = f"Can you write a poem about {topic} in {WordCount} words?"
+     
+     llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+     response = llm.invoke(prompt)
+     
+     st.subheader("Generated Poem:")
+     st.write(response.content)
+
+
+
+
+
+
+# llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+# response = llm.invoke("Can you write a poem about a sunset in 50 words?")
+# print(response.content)
+
+# # adding streamlit interface
+
+# st.title("Google Gemini 2.5 Flash Poem Generator")
+# user_input = st.text_input("Enter your prompt:", "Can you write a poem about a sunset in 50 words?")
+# if st.button("Generate Poem"):
+#     response = llm.invoke(user_input)
+#     st.write(response.content)  
+
